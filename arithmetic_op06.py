93e543d154d56222c2d9640e6ebf3ae06e51fee8
Akbar Abdullayev <abdullayevakbar0101@gmail.com>
2022-06-15 16:02:45 +0500

    arithmetic_op

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -3,7 +3,7 @@ number = 15
 # Divide the number by 2 and assign number.
 number = number/2
 # Find the fraction of the number and assing to a variable called 'answer'.
-answer = (number)//1
+answer = number-(number)//1
 # Print the answer.
 print(answer)
 print(number)
