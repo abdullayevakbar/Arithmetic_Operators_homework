55d5b73b61e8a6478b59547e145e96d8821e61c5
Akbar Abdullayev <abdullayevakbar0101@gmail.com>
2022-06-15 16:01:09 +0500

    arithmetic_op

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -5,9 +5,9 @@ x1 = number % 10
 # Find the 'number' second digit and assign to x2.
 x2 = number//10 % 10
 x3 = number//100
-print(x1+x2+x3)
+answer = x1+x2+x3
 # Find the 'number' third digit and assign to x3.
 
 # Create a variable called 'answer' and assign it the sum of the three digits.
-
+print(answer)
 # print the sum of the three digits.
