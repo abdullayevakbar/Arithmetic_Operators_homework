34ce5ae7ca5fd87edf54cffe867b3d3e96c6a2f4
Akbar Abdullayev <abdullayevakbar0101@gmail.com>
2022-06-15 15:58:10 +0500

    arithmetic_op

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,4 @@
 # Create a variable called 'number' and assign it the two-digit number
-from multiprocessing.connection import answer_challenge
-
-
 number = 12
 # Find the reverse of the number and assign it to a variable called 'answer'.
 answer = number//10+number % 10*10
